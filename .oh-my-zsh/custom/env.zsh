d998e6411c874ac912d04b743a7fed0d96010ab2
Nate Pinsky <n.j.pinsky@gmail.com>
2018-07-16 13:55:05 -0700

    Add zmv to zsh setup

--- .oh-my-zsh/custom/env.zsh
+++ .oh-my-zsh/custom/env.zsh
@@ -1,2 +1,8 @@
 export EDITOR=vim
 export PATH="$HOME/.pollev/bin:$HOME/.local/bin:$PATH"
+
+autoload zmv
+alias zmv='noglob zmv'
+alias zcp='noglob zmv -C'
+alias zln='noglob zmv -L'
+alias zsy='noglob zmv -Ls'
